2d869af0a1fc9e8806a73384fae416cf52f21c50
andrewcbuensalida <andrewcbuensalida@gmail.com>
2022-05-08 16:45:08 -0700

    Initial, ec2, no .exe

---
+++ README.md
@@ -0,0 +1,7 @@
+https://www.youtube.com/watch?v=SLB_c_ayRMo&t=2717s
+
+for terraform cloud
+https://learn.hashicorp.com/tutorials/terraform/aws-remote?in=terraform/aws-get-started
+
+
+
---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  cloud {
+    organization = "portfolio-1"
+    workspaces {
+      name = "portfolio-1-workspace"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-west-1"
+}
+
+resource "aws_instance" "portfolio-1" {
+  ami           = "ami-01f87c43e618bf8f0"
+  instance_type = "t2.micro"
+
+# to install code pipeline in the ec2
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt -y update
+              sudo apt -y install ruby
+              sudo apt -y install wget
+              cd /home/ubuntu
+              wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+              sudo chmod +x ./install
+              sudo ./install auto
+              curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+              . ~/.nvm/nvm.sh
+              nvm install node
+              EOF
+
+  tags = {
+    Name = "portfolio-1"
+  }
+}
