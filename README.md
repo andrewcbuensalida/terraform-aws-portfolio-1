82e3e3c55c7655184186fa1aa4585b8734c3786e
andrewcbuensalida <andrewcbuensalida@gmail.com>
2022-05-09 08:25:47 -0700

    key_name in aws_instance resource is a string, not from key_pair resource

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ https://learn.hashicorp.com/tutorials/terraform/aws-remote?in=terraform/aws-get-
 
 workflow: just push or pr to remote master in gh, terraform cloud should detect changes then auto apply.
 
-the other workflow is, just doing terraform apply on local, then it will update terraform cloud, but have to setup cloud to do it this way. cant have github workflow and local cli workflow at the same time.
+the other workflow is, 
+    terraform apply
+on local, then it will update terraform cloud, but have to setup cloud to do it this way. cant have github workflow and local cli workflow at the same time.
 
 for the first time auto deploying from gh to tf cloud, have to do it manually.
--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
       name = "terraform-aws-portfolio-1"
     }
   }
-
+# so terraform knows what .exe to download from the registry
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,10 +13,10 @@ terraform {
     }
   }
 }
-
+# each provider must be declared in the required providers section above
 provider "aws" {
-  # profile = "default" //for local to tf cloud, wont work with gh to tf cloud workflow
-  region = "us-west-1"
+  # profile = "default" //for local to tf cloud, wont work with gh to tf cloud workflow. default pulls the credentials from aws-cli. with gh to cloud workflow, it gets the access_key and secret_key from the variables you set in cloud.
+  region = "us-west-1a"
 
 }
 
@@ -24,7 +24,7 @@ provider "aws" {
 resource "aws_instance" "portfolio-1" {
   ami           = "ami-01f87c43e618bf8f0"
   instance_type = "t2.micro"
-
+  key_name = "ec2-key-pair-portfolio-1"
 # to install code pipeline in the ec2
   user_data = <<-EOF
               #!/bin/bash
@@ -44,3 +44,10 @@ resource "aws_instance" "portfolio-1" {
     Name = "portfolio-1b"
   }
 }
+
+resource "aws_key_pair" "ec2-key-pair-portfolio-1" {
+  key_name   = "ec2-key-pair-portfolio-1"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCu1w50qkgocp5k2KPoi84/Kp2FRJ960ZPDyeSoiEskWD64ZuyXacvp4t8O6Rc/pr1xrUZjrbQtwSsJ/6Z1dN/JF4KVJSAuJwWXu7iZtYnDZ13pVpu8nI5R45NfVET52O2refvJoOgWE6T6npui6Ow85IDywPj4klMDVTbmUNj4wty4hV7RrzNj/0zi9SJ76xITvSOnGOzAPtcT+zOrrRbpnyUbPGXrdDzPau1D/K4FQe2Sam6NDxV4KpC7Cq4Y1p01JsTGcXgGwcNav2Dx6sZmgYykHpfi2x36sJ1BW87AM9bfhEEKgvRCJWik1tHC3GdGORcajzAZgAHmKpPt4IdN imported-openssh-key"
+}
+
+
