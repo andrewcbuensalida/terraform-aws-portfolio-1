de505b32f6b287c39a18c314432ed506ca1b7d81
andrewcbuensalida <andrewcbuensalida@gmail.com>
2022-05-09 09:33:23 -0700

    with security group, and not creating key pair but just referencing the name

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ the other workflow is,
 on local, then it will update terraform cloud, but have to setup cloud to do it this way. cant have github workflow and local cli workflow at the same time.
 
 for the first time auto deploying from gh to tf cloud, have to do it manually.
+
+/////////////////////////////////////////
+
+Having an error when ssh
+
+At least it created the instance this time, but then it also created a new key pair.
+
+ssh -i "ec2.pem" ubuntu@ec2-54-151-8-143.us-west-1.compute.amazonaws.com
+Error says connection time out. Can't connect through aws console either. 
+So I manually edited inbound rules of the security group to allow ssh, http, and https.
+Now in command line I can connect via ssh. Can also connect via console connect.
--- main.tf
+++ main.tf
@@ -24,7 +24,10 @@ provider "aws" {
 resource "aws_instance" "portfolio-1" {
   ami           = "ami-01f87c43e618bf8f0"
   instance_type = "t2.micro"
-  key_name = "ec2-key-pair-portfolio-1"
+  # key_name = data.aws_key_pair.ec2-key-pair-portfolio-1.key_name
+  key_name = "ec2"
+  availability_zone = "us-west-1a"
+  security_groups = [ "MY_DEFAULT" ]
 # to install code pipeline in the ec2
   user_data = <<-EOF
               #!/bin/bash
@@ -45,9 +48,19 @@ resource "aws_instance" "portfolio-1" {
   }
 }
 
-resource "aws_key_pair" "ec2-key-pair-portfolio-1" {
-  key_name   = "ec2-key-pair-portfolio-1"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCu1w50qkgocp5k2KPoi84/Kp2FRJ960ZPDyeSoiEskWD64ZuyXacvp4t8O6Rc/pr1xrUZjrbQtwSsJ/6Z1dN/JF4KVJSAuJwWXu7iZtYnDZ13pVpu8nI5R45NfVET52O2refvJoOgWE6T6npui6Ow85IDywPj4klMDVTbmUNj4wty4hV7RrzNj/0zi9SJ76xITvSOnGOzAPtcT+zOrrRbpnyUbPGXrdDzPau1D/K4FQe2Sam6NDxV4KpC7Cq4Y1p01JsTGcXgGwcNav2Dx6sZmgYykHpfi2x36sJ1BW87AM9bfhEEKgvRCJWik1tHC3GdGORcajzAZgAHmKpPt4IdN imported-openssh-key"
-}
+# this is to create a new key pair, but should already have .pem
+# resource "aws_key_pair" "ec2-key-pair-portfolio-1" {
+#   key_name   = "ec2-key-pair-portfolio-1a"
+#   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCu1w50qkgocp5k2KPoi84/Kp2FRJ960ZPDyeSoiEskWD64ZuyXacvp4t8O6Rc/pr1xrUZjrbQtwSsJ/6Z1dN/JF4KVJSAuJwWXu7iZtYnDZ13pVpu8nI5R45NfVET52O2refvJoOgWE6T6npui6Ow85IDywPj4klMDVTbmUNj4wty4hV7RrzNj/0zi9SJ76xITvSOnGOzAPtcT+zOrrRbpnyUbPGXrdDzPau1D/K4FQe2Sam6NDxV4KpC7Cq4Y1p01JsTGcXgGwcNav2Dx6sZmgYykHpfi2x36sJ1BW87AM9bfhEEKgvRCJWik1tHC3GdGORcajzAZgAHmKpPt4IdN imported-openssh-key"
+# }
+
+# this is to get an existing key pair. the aws in aws_key_pair is the provider, the key_pair is the type of resource, ec2-key-pair-portfolio-1 is a custom name used only in these files. I don't even know if I need this.
+# data "aws_key_pair" "ec2-key-pair-portfolio-1" {
+#   key_name = "ec2"
+#   filter {
+#     name   = "tag:portfolio"
+#     values = ["1"]
+#   }
+# }
 
 
